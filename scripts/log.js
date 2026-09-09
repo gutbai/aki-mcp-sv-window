ceddcce30878d9042e8e0b08dd3baf1362e48029
Nguyen Nguyen <68273578+gutbai@users.noreply.github.com>
2026-09-09 10:17:31 +0700

    fix: redact only actual secret query/form parameters

--- scripts/log.js
+++ scripts/log.js
@@ -21,7 +21,7 @@ const REDACT_KEY = /authorization|cookie|set-cookie|client[_-]?secret|access[_-]
 function redactString(value) {
   return value
     .replace(/(Bearer\s+)[A-Za-z0-9._~+/=-]+/gi, '$1[REDACTED]')
-    .replace(/((?:passphrase|client_secret|access_token|refresh_token|code_verifier|x-panel-token|token|password|code|t)=)[^&\s"']+/gi, '$1[REDACTED]');
+    .replace(/(^|[?&])((?:passphrase|client_secret|access_token|refresh_token|code_verifier|x-panel-token|token|password|code|t)=)[^&\s"']+/gi, '$1$2[REDACTED]');
 }
 
 function sanitize(value, seen = new WeakSet(), key = '') {
